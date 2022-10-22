67bb80f4d1cddd7627c76ec978dc46c3a1ee1794
Ikhwan <nsetyo19@gmail.com>
2022-10-23 04:20:35 +0700

    Initial Commit

---
+++ Mini Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Task
+//
+//  Created by Ikhwan on 23/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
